8a7f60f04494127f16f6463b5021c71bb90d4bed
Mat Whiteside <matwhiteside1@gmail.com>
2019-08-04 13:40:47 +0100

    Added folder structure for pygame, added code for first two python tutorials

---
+++ Beginner Python Tutorials/0. Introduction/Name example.py
@@ -0,0 +1,2 @@
+first_name = input("What is your name? Enter: ")
+print("Hello", first_name)
---
+++ Beginner Python Tutorials/1. Hello, World!/Main.py
@@ -0,0 +1,7 @@
+print("Hello, world!")
+
+print("This is a single message")
+
+print("This is the first message", "This is the second message")
+
+print("This is the first message", "This is the second message", sep=" / ")
