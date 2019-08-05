f59f66b7daa6af8e451f58d6b27c15e809e7017c
Mat Whiteside <matwhiteside1@gmail.com>
2019-08-05 14:10:46 +0100

    Uploaded examples for data types post

---
+++ Beginner Python Tutorials/2. Data types/Examples.py
@@ -0,0 +1,44 @@
+# Note that a line like this is a comment, it is not
+# valid code and python completetly ignores it. You can
+# write what you want!
+
+# String examples
+print("This is a string")
+print('This is a string')
+print("This is a string containing the number 42")
+print("This is a string containing 'A quote inside of it'")
+
+# String example with escape character
+print("This is a string containing \"A quote inside of it\"")
+print('We can do the same with single quotes \'Like this!\'')
+print("We can also display backslashes \\")
+print("Or we can create new lines!\nThis is on a new line")
+print("...Maybe we even want to create a tab character:\ttext after the tab")
+
+# Integer examples
+print(42)
+print(-42)
+print(0)
+print(424242424242424242424242424242424242424242424242424242424242)
+
+# Float examples
+print(4.2)
+print(42.0)
+print(-42.0)
+print(42e1)
+
+# Complex examples
+print(42+5j)
+print(42+0j)
+print(0+42j)
+
+# Boolean examples
+print(True)
+print(False)
+
+# Examples of type() function
+print(type("Hello!"))
+print(type(42))
+print(type(4.2))
+print(type(42+5j))
+print(type(True))
